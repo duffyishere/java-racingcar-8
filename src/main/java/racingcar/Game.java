2dc26440afe3a81cee8efdc5e4902c9e6440ad47
zzunho9 <jamlove4763@naver.com>
2025-10-27 16:26:53 +0900

    refactor: 유저 입출력 기능과 비즈니스 로직 간의 코드 분리

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,81 +1,46 @@
 package racingcar;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Game {
-    private final Map<String, Integer> playerLevels = new HashMap<>();
-    private int maxRound = 0;
-    private int currentRound = 1;
+    private final Map<String, Integer> playerStatus = new LinkedHashMap<>();
 
-    public void start() {
-        setup();
-        while(this.currentRound <= this.maxRound) {
-            playRound();
-            printProgress();
-            this.currentRound++;
-        }
-        printWinner();
-    }
-
-    private void setup() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String playerLevelsInput = Console.readLine();
-        for (String player: playerLevelsInput.split(",")) {
-            player = player.trim();
-            if (playerLevels.containsKey(player))
-                throw new IllegalArgumentException("이미 등록된 자동차 이름입니다.");
-            if (4 < player.length())
-                throw new IllegalArgumentException("자동차 이름은 5글자 이하만 가능합니다.");
-            playerLevels.put(player, 0);
-        }
-
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        String maxRoundInput = Console.readLine().trim();
-        try {
-            this.maxRound = Integer.parseInt(maxRoundInput);
-            if (maxRound < 1)
-                throw new IllegalArgumentException("진행할 라운드 수는 0보다 커야합니다.");
-        } catch (NumberFormatException exception) {
-                throw new IllegalArgumentException("진행할 라운드 수는 숫자만 입력 가능합니다.");
+    public Game(List<String> playerNames) {
+        for (String player: playerNames) {
+            validateName(player);
+            playerStatus.put(player, 0);
         }
     }
 
-    private void playRound() {
-        for (String player: playerLevels.keySet()) {
+    private void validateName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다: " + name);
+        }
+        if (playerStatus.containsKey(name)) {
+            throw new IllegalArgumentException("이미 등록된 자동차 이름입니다: " + name);
+        }
+    }
+
+    public void playRound() {
+        for (String player: playerStatus.keySet()) {
             if (canGoNext()) {
-                int nextLevel = playerLevels.get(player) + 1;
-                playerLevels.put(player, nextLevel);
+                int nextLevel = playerStatus.get(player) + 1;
+                playerStatus.put(player, nextLevel);
             }
         }
     }
 
-    private boolean canGoNext() {
-        int value = Randoms.pickNumberInRange(0, 9);
-        return 3 < value;
+    public Map<String, Integer> getPlayerStatus() {
+        return playerStatus;
     }
 
-    private void printProgress() {
-        StringBuilder sb = new StringBuilder();
-        for (String player: playerLevels.keySet()) {
-            int level = playerLevels.get(player);
-            sb.append(player).append(" : ");
-            sb.append("-".repeat(Math.max(0, level)));
-            sb.append("\n");
-        }
-        System.out.println(sb);
-    }
-
-    private List<String> findWinner() {
+    public List<String> findWinner() {
         List<String> winners = new ArrayList<>();
         int maxLevel = 0;
-        for (String player: playerLevels.keySet()) {
-            int level = playerLevels.get(player);
+        for (String player: playerStatus.keySet()) {
+            int level = playerStatus.get(player);
             if (maxLevel < level) {
                 maxLevel = level;
                 winners.clear();
@@ -87,11 +52,8 @@ public class Game {
         return winners;
     }
 
-    private void printWinner() {
-        List<String> winners = findWinner();
-        StringBuilder sb = new StringBuilder();
-        sb.append("최종 우승자 : ");
-        winners.forEach(winner -> sb.append(winner).append(", "));
-        System.out.println(sb.substring(0, sb.length() - 2));
+    private boolean canGoNext() {
+        int value = Randoms.pickNumberInRange(0, 9);
+        return 3 < value;
     }
 }
---
+++ src/main/java/racingcar/GameController.java
@@ -0,0 +1,65 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class GameController {
+    public void start() {
+        List<String> playerNames = readPlayerNames();
+        int maxRound = readMaxRound();
+        Game game = new Game(playerNames);
+
+        for (int round = 1; round <= maxRound; round++) {
+            game.playRound();
+            printProgress(game.getPlayerStatus());
+        }
+
+        printWinners(game.findWinner());
+    }
+
+    public List<String> readPlayerNames() {
+        List<String> playerNames = new ArrayList<>();
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String playerLevelsInput = Console.readLine();
+        for (String player: playerLevelsInput.split(",")) {
+            String trimmedPlayerName = player.trim();
+            validatePlayerName(playerNames, trimmedPlayerName);
+            playerNames.add(trimmedPlayerName);
+        }
+        return playerNames;
+    }
+
+    private void validatePlayerName(List<String> playerLevels, String player) {
+        if (playerLevels.contains(player))
+            throw new IllegalArgumentException("이미 등록된 자동차 이름입니다.");
+        if (4 < player.length())
+            throw new IllegalArgumentException("자동차 이름은 5글자 이하만 가능합니다.");
+    }
+
+    public int readMaxRound() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String maxRoundInput = Console.readLine().trim();
+        try {
+            int maxRound = Integer.parseInt(maxRoundInput);
+            if (maxRound < 1)
+                throw new IllegalArgumentException("진행할 라운드 수는 0보다 커야합니다.");
+            return maxRound;
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("진행할 라운드 수는 숫자만 입력 가능합니다.");
+        }
+    }
+
+    private void printProgress(Map<String, Integer> playerStatus) {
+        playerStatus.forEach((player, level) -> {
+            System.out.println(player + " : " + "-".repeat(level));
+        });
+        System.out.println();
+    }
+
+    private void printWinners(List<String> winners) {
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+}
